47e6ee66f3ac943224de68e99e2057734ddecd84
oparnier <oparnier@4ce31ec0-1c2c-0410-8017-ff43ee565779>
2008-05-13 14:39:56 +0000

    change header's extension from 'SK.cc' to 'SK.cpp'
    git-svn-id: file:///home/raph/download/milimail/trunk@674 4ce31ec0-1c2c-0410-8017-ff43ee565779

--- message_remote_services/scripts/generate_corba_stubs.sh
+++ message_remote_services/scripts/generate_corba_stubs.sh
@@ -1,1 +1,1 @@
-../lib/omniORB-4.1.2/bin/omniidl -C ../src/corba/ -bcxx -Wbh=.h ../src/corba/Services.idl
+../lib/omniORB-4.1.2/bin/omniidl -C ../src/corba/ -bcxx -Wbh=.h -Wbs=SK.cpp ../src/corba/Services.idl
