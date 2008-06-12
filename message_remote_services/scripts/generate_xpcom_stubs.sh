b2b6a12972e8d29d33ffe19f69de4c83665fa4c4
oparnier <oparnier@4ce31ec0-1c2c-0410-8017-ff43ee565779>
2008-06-12 10:39:32 +0000

    Add bash verbose mode
    git-svn-id: file:///home/raph/download/milimail/trunk@739 4ce31ec0-1c2c-0410-8017-ff43ee565779

--- message_remote_services/scripts/generate_xpcom_stubs.sh
+++ message_remote_services/scripts/generate_xpcom_stubs.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-MOZILLA_SDK_PATH=~/workspace/mozilla-2.0/dist/sdk/bin/
-MOZILLA_SDK_INCLUDE_PATH=~/workspace/mozilla-2.0/dist/sdk/idl/
+set -x
 
+MOZILLA_SDK_PATH=~/workspace/mozilla/dist/sdk/bin/
+MOZILLA_SDK_INCLUDE_PATH=~/workspace/mozilla/dist/sdk/idl/
 
 $MOZILLA_SDK_PATH/xpidl -e ../src/xpcom/IMessageRemoteService.h -m header \
  -I$MOZILLA_SDK_INCLUDE_PATH ../src/xpcom/IMessageRemoteService.idl
  
  $MOZILLA_SDK_PATH/xpidl -e ../xpi/components/IMessageRemoteService.xpt -m typelib \
  -I$MOZILLA_SDK_INCLUDE_PATH ../src/xpcom/IMessageRemoteService.idl
-mkdir dist 
- echo "[INFO] ../src/xpcom/IMessageRemoteService.h created from ../src/xpcom/IMessageRemoteService.idl"
- echo "[INFO] IMessageRemoteService.xpt created from ../src/xpcom/IMessageRemoteService.idl"
+ 
