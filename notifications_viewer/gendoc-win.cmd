67562b306bebe2d2afd28b590220938d0eb671e5
olivierbrun <olivierbrun@4ce31ec0-1c2c-0410-8017-ff43ee565779>
2008-09-25 09:38:18 +0000

    Rename gendoc.sh to gendoc-linux.sh Add JavaScript Doc for windows with gendoc-win.cmd Add for the both project logo in the documentation.
    git-svn-id: file:///home/raph/download/milimail/trunk@1117 4ce31ec0-1c2c-0410-8017-ff43ee565779

---
+++ notifications_viewer/gendoc-win.cmd
@@ -0,0 +1,30 @@
+@echo off
+rem Command file
+rem generate doc with JSDoc
+rem
+rem Download JSDoc script: http://jsdoc.sourceforge.net/#install
+rem and install it.
+rem Change JsDocPath variable
+rem
+rem Olivier Brun - 2008-09-25
+
+rem path for JSDoc
+set JsDocPath=C:\Tools\JSDoc\jsdoc.pl
+
+rem Project logo
+set logoPath=.\chrome\skin\notificationsviewer.png
+
+rem project summary file
+set projectSummary=project_summary.txt
+
+rem document files
+set files=.\chrome\content
+
+rem output directory
+set outputDirectory=doc
+
+
+rem Clean up output directory
+if exist %outputDirectory% rmdir %outputDirectory% /S /Q 
+
+perl %JsDocPath% -r -d %outputDirectory% --logo %logoPath% --project-summary %projectSummary%  --no-sources %files%
