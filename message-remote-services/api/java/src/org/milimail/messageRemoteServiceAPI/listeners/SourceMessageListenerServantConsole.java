3f2393df0ebf02142c2d4334a4afdb9bb814af41
oparnier <oparnier@4ce31ec0-1c2c-0410-8017-ff43ee565779>
2008-12-08 09:13:50 +0000

    erase @override cannot compile on Forge
    git-svn-id: svn+ssh://svn.adullact.net/svnroot/milimail/trunk@1217 4ce31ec0-1c2c-0410-8017-ff43ee565779

--- message-remote-services/api/java/src/org/milimail/messageRemoteServiceAPI/listeners/SourceMessageListenerServantConsole.java
+++ message-remote-services/api/java/src/org/milimail/messageRemoteServiceAPI/listeners/SourceMessageListenerServantConsole.java
@@ -4,11 +4,8 @@ import org.milimail.messageRemoteServiceAPI.stubs.SourceMessageListenerPOA;
 
 public class SourceMessageListenerServantConsole extends SourceMessageListenerPOA {
 
-	@Override
 	public void OnLoad(String source) {
-		System.out.println("OnLoad");
 		System.out.println(source);
-		
 	}
 
 	
