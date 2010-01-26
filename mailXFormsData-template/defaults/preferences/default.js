ec87482f6e8725daafe5de35e702ba91985c067a
rfairise <rfairise@4ce31ec0-1c2c-0410-8017-ff43ee565779>
2010-01-26 14:29:03 +0000

    add Mail XForms Data template
    git-svn-id: svn+ssh://svn.adullact.net/svnroot/milimail/trunk@1416 4ce31ec0-1c2c-0410-8017-ff43ee565779

---
+++ mailXFormsData-template/defaults/preferences/default.js
@@ -0,0 +1,11 @@
+# Form URL in current package (mandatory)
+#  Remplace "myFormName" with the unique name of the form (a-z, A-Z, 0-9, without spaces)
+#  Replace "1" with the version of the form ("1", "2.3", ...)
+#  Replace "myForm.xul" with the file name of the form
+#pref("extensions.mailXFormsEngine.forms.myFormName.1.url", "chrome://mailXFormsData-MXD_PACKAGE_NAME/content/myForm.xul");
+
+# Form display name (default to the unique name of the form)
+#pref("extensions.mailXFormsEngine.forms.myFormName.1.title", "My Form title");
+
+# Should the form be available when composing a message (defaults to true)
+#pref("extensions.mailXFormsEngine.forms.myFormName.1.composeEnabled", false);
