7f0e6cde48eda62e3e4377253d06a7f86a714e59
rfairise <rfairise@4ce31ec0-1c2c-0410-8017-ff43ee565779>
2010-01-29 10:25:17 +0000

    Add XIMF sample form
    git-svn-id: svn+ssh://svn.adullact.net/svnroot/milimail/trunk@1419 4ce31ec0-1c2c-0410-8017-ff43ee565779

--- mailXFormsData-sample/defaults/preferences/default.js
+++ mailXFormsData-sample/defaults/preferences/default.js
@@ -1,14 +1,13 @@
-pref("extensions.mailXFormsEngine.forms.sampleA.1.title", "Sample 中文(简)  中文(HK)  中文(繁)€ A1");
+# Form URL in current package (mandatory)
+pref("extensions.mailXFormsEngine.forms.ximf.0.1.url", "chrome://mailXFormsData-sample/content/XIMF.xul");
+# Form display name (default to the unique name of the form)
+pref("extensions.mailXFormsEngine.forms.ximf.0.1.title", "XIMF");
+# Should the form be available when composing a message (defaults to true)
+#pref("extensions.mailXFormsEngine.forms.ximf.0.1.composeEnabled", true);
+
+# Form URL in current package (mandatory)
 pref("extensions.mailXFormsEngine.forms.sampleA.1.url", "chrome://mailXFormsData-sample/content/formSampleA1.xul");
-pref("extensions.mailXFormsEngine.forms.sampleA.1.composeEnabled", false);
-
-pref("extensions.mailXFormsEngine.forms.sampleA.2.4.title", "nom du formulaire");
-pref("extensions.mailXFormsEngine.forms.sampleA.2.4.url", "chrome://mailXFormsData-sample/content/formSampleA2.xul");
-
-pref("extensions.mailXFormsEngine.forms.sampleC.1.url", "chrome://mailXFormsData-sample/content/formSampleC1.xul");
-
-pref("extensions.mailXFormsEngine.forms.sampleC.3.title", "nom du formulaireC");
-pref("extensions.mailXFormsEngine.forms.sampleC.3.url", "chrome://mailXFormsData-sample/content/formSampleC3.xul");
-
-pref("extensions.mailXFormsEngine.forms.sample-XUL.1.url", "chrome://mailXFormsData-sample/content/formSampleD.xul");
-pref("extensions.mailXFormsEngine.forms.sample-XHTML.1.url", "chrome://mailXFormsData-sample/content/range-bind.xhtml");
+# Form display name (default to the unique name of the form)
+pref("extensions.mailXFormsEngine.forms.sampleA.1.title", "Sample A");
+# Should the form be available when composing a message (defaults to true)
+#pref("extensions.mailXFormsEngine.forms.sampleA.1.composeEnabled", true);
