4a636b4b894b0ea5016c874b9beea769f260285e
rfairise <rfairise@4ce31ec0-1c2c-0410-8017-ff43ee565779>
2010-03-15 16:49:29 +0000

    fix comments
    git-svn-id: svn+ssh://svn.adullact.net/svnroot/milimail/trunk@1442 4ce31ec0-1c2c-0410-8017-ff43ee565779

--- mailXFormsData-sample/defaults/preferences/default.js
+++ mailXFormsData-sample/defaults/preferences/default.js
@@ -1,13 +1,13 @@
-# Form URL in current package (mandatory)
+/* Form URL in current package (mandatory) */
 pref("extensions.mailXFormsEngine.forms.ximf.0.1.url", "chrome://mailXFormsData-sample/content/XIMF.xul");
-# Form display name (default to the unique name of the form)
+/* Form display name (default to the unique name of the form) */
 pref("extensions.mailXFormsEngine.forms.ximf.0.1.title", "XIMF");
-# Should the form be available when composing a message (defaults to true)
-#pref("extensions.mailXFormsEngine.forms.ximf.0.1.composeEnabled", true);
+/* Should the form be available when composing a message (defaults to true) */
+//pref("extensions.mailXFormsEngine.forms.ximf.0.1.composeEnabled", true);
 
-# Form URL in current package (mandatory)
+/* Form URL in current package (mandatory) */
 pref("extensions.mailXFormsEngine.forms.sampleA.1.url", "chrome://mailXFormsData-sample/content/formSampleA1.xul");
-# Form display name (default to the unique name of the form)
+/* Form display name (default to the unique name of the form) */
 pref("extensions.mailXFormsEngine.forms.sampleA.1.title", "Sample A");
-# Should the form be available when composing a message (defaults to true)
-#pref("extensions.mailXFormsEngine.forms.sampleA.1.composeEnabled", true);
+/* Should the form be available when composing a message (defaults to true) */
+//pref("extensions.mailXFormsEngine.forms.sampleA.1.composeEnabled", true);
