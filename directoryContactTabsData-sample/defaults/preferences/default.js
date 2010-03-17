f7b5f8f2cb4dd076ac6329a02a3699d0877a31f8
rfairise <rfairise@4ce31ec0-1c2c-0410-8017-ff43ee565779>
2010-03-17 09:44:58 +0000

    doc
    git-svn-id: svn+ssh://svn.adullact.net/svnroot/milimail/trunk@1448 4ce31ec0-1c2c-0410-8017-ff43ee565779

--- directoryContactTabsData-sample/defaults/preferences/default.js
+++ directoryContactTabsData-sample/defaults/preferences/default.js
@@ -1,5 +1,14 @@
-pref("extensions.directoryContactTabs.sample1.tab.url", "chrome://directoryContactTabsData-sample/content/sample1.xul");
-pref("extensions.directoryContactTabs.sample1.tab.title", "Sample tab 1");
+/* Register all panels provided by this add-on */
 
+/*
+ * .url preference defines the location of the XUL file in this add-on
+ * .title preference defines the name displayed in the tab panel
+ */
+
+/* Panel sample1.xul */
+pref("extensions.directoryContactTabs.sample1.tab.url", "chrome://directoryContactTabsData-sample/content/sample1.xul");
+pref("extensions.directoryContactTabs.sample1.tab.title", "Sample 1");
+
+/* Panel sample2.xul */
 pref("extensions.directoryContactTabs.sample2.tab.url", "chrome://directoryContactTabsData-sample/content/sample2.xul");
-pref("extensions.directoryContactTabs.sample2.tab.title", "Sample tab 2");
+pref("extensions.directoryContactTabs.sample2.tab.title", "Sample 2");
--- directoryContactTabsData-template/defaults/preferences/default.js
+++ directoryContactTabsData-template/defaults/preferences/default.js
@@ -1,5 +1,15 @@
-//pref("extensions.directoryContactTabs.tab1.tab.url", "chrome://directoryContactTabsData-DCT_PACKAGE_NAME/content/sample1.xul");
-//pref("extensions.directoryContactTabs.tab1.tab.title", "Sample tab 1");
+/* Register all panels provided by this add-on */
 
-//pref("extensions.directoryContactTabs.tab2.tab.url", "chrome://directoryContactTabsData-DCT_PACKAGE_NAME/content/sample2.xul");
-//pref("extensions.directoryContactTabs.tab2.tab.title", "Sample tab 2");
+/*
+ * .url preference defines the location of the XUL file in this add-on
+ * .title preference defines the name displayed in the tab panel
+ */
+
+//pref("extensions.directoryContactTabs.DCT_PACKAGE_NAME1.tab.url", "chrome://directoryContactTabsData-DCT_PACKAGE_NAME/content/filename1.xul");
+//pref("extensions.directoryContactTabs.DCT_PACKAGE_NAME1.tab.title", "Tab 1");
+
+//pref("extensions.directoryContactTabs.DCT_PACKAGE_NAME2.tab.url", "chrome://directoryContactTabsData-DCT_PACKAGE_NAME/content/filename2.xul");
+//pref("extensions.directoryContactTabs.DCT_PACKAGE_NAME2.tab.title", "Tab 2");
+
+//pref("extensions.directoryContactTabs.DCT_PACKAGE_NAME3.tab.url", "chrome://directoryContactTabsData-DCT_PACKAGE_NAME/content/filename3.xul");
+//pref("extensions.directoryContactTabs.DCT_PACKAGE_NAME3.tab.title", "Tab 3");
