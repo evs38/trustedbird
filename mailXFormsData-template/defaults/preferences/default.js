b3ea6e8782fe42a41c5657e6ba6db61d7f80a189
rfairise <rfairise@4ce31ec0-1c2c-0410-8017-ff43ee565779>
2010-03-17 09:44:55 +0000

    doc
    git-svn-id: svn+ssh://svn.adullact.net/svnroot/milimail/trunk@1447 4ce31ec0-1c2c-0410-8017-ff43ee565779

--- mailXFormsData-template/defaults/preferences/default.js
+++ mailXFormsData-template/defaults/preferences/default.js
@@ -1,7 +1,8 @@
 /* Form URL in current package (mandatory)
-    Replace "myFormName" with the unique name of the form (a-z, A-Z, 0-9, without spaces)
-    Replace "1" with the version of the form ("1", "2.3", ...)
-    Replace "myForm.xul" with the file name of the form */
+ *   Replace "myFormName" with the unique name of the form (a-z, A-Z, 0-9, without spaces)
+ *   Replace "1" with the version of the form ("1", "2.3", ...)
+ *   Replace "myForm.xul" with the file name of the form
+ */
 //pref("extensions.mailXFormsEngine.forms.myFormName.1.url", "chrome://mailXFormsData-MXD_PACKAGE_NAME/content/myForm.xul");
 
 /* Form display name (default to the unique name of the form) */
